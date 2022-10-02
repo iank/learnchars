9e1911d93f4d2caae961c8617e3e4a8d0e1b2b77
Ian Kilgore <iank@iank.org>
2022-10-02 08:22:27 -0400

    Utility script: fetch more than one character

--- scripts/get_vocab.py
+++ scripts/get_vocab.py
@@ -11,22 +11,34 @@ from learnchars.skritter import import_from_tsv
 from learnchars.chars import get_next_character
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        sys.exit("Usage: {} filename.tsv".format(sys.argv[0]))
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
+        sys.exit("Usage: {} filename.tsv [n]".format(sys.argv[0]))
 
     tsv_file = Path(sys.argv[1])
     chars = import_from_tsv(tsv_file)
-    (next_char, rank) = get_next_character(chars)
 
-    print("Next unknown character: {} (rank: {})".format(next_char, rank))
+    if len(sys.argv) == 3:
+        try:
+            n = int(sys.argv[2])
+        except ValueError as e:
+            sys.exit("Second argument must be numeric")
+    else:
+        n = 1
 
-    found_words = 0
-    for word in wordfreq.iter_wordlist('zh'):
-        if next_char in word:
-            found_words = found_words + 1
-            print("#{}:\t{}\t({})".format(
-                found_words, word, wordfreq.word_frequency(word, "zh"))
-            )
+    if n < 1:
+        sys.exit("n must be >= 1")
 
-        if found_words == 5:
-            break
+    next_chars = get_next_character(chars, n)
+    for (next_char, rank) in next_chars:
+        print("Next unknown character: {} (rank: {})".format(next_char, rank))
+
+        found_words = 0
+        for word in wordfreq.iter_wordlist('zh'):
+            if next_char in word:
+                found_words = found_words + 1
+                print("#{}:\t{}\t({})".format(
+                    found_words, word, wordfreq.word_frequency(word, "zh"))
+                )
+
+            if found_words == 5:
+                break
