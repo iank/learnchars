3235299f3609e5ad4b3301050ce0cb5a98a4eef5
Ian Kilgore <iank@iank.org>
2022-10-10 16:23:20 -0400

    break up skritter import function

--- src/learnchars/skritter.py
+++ src/learnchars/skritter.py
@@ -2,15 +2,18 @@ import re
 
 
 def import_from_tsv(filename):
-    lines = []
-    with open(filename) as f:
-        lines = f.readlines()
-
-    valid_lines = [line for line in lines if re.match(".*\t.*\t.*\t", line)]
-    words = [line.split("\t")[0] for line in valid_lines]
-
+    words = words_from_tsv(filename)
     chars = []
     for word in words:
         chars.extend([*word])
 
     return set(chars)
+
+
+def words_from_tsv(filename):
+    lines = []
+    with open(filename) as f:
+        lines = f.readlines()
+
+    valid_lines = [line for line in lines if re.match(".*\t.*\t.*\t", line)]
+    return [line.split("\t")[0] for line in valid_lines]
